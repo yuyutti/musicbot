79499fe3bb27378e8b0f1ea3a6657524bc6bde0a
yuyutti <yuyutti@yuyutti.com>
2024-07-05 13:43:38 +0900

    feat: Add express.js file for handling data endpoint

--- src/playingGuild.js
+++ src/playingGuild.js
@@ -3,6 +3,8 @@ const { getStatusChannel, getStatusMessage, getLoggerChannel, getErrorChannel } 
 const { queue } = require('./musicQueue');
 const { updateActivity } = require('./activity');
 
+require('../src/express');
+
 function updatePlayingGuild() {
     const statusChannel = getStatusChannel();
     const statusMessage = getStatusMessage();
@@ -10,8 +12,11 @@ function updatePlayingGuild() {
     const errorChannel = getErrorChannel();
     if (!statusChannel) return loggerChannel.send('Statusチャンネルに接続できませんでした');
 
+    process.customData = [];
+
     const mapSize = queue.size;
     let listener = 0;
+    let membersInfo = [];
 
     let hasValidServer = false;
     const embed = new EmbedBuilder()
@@ -28,6 +33,10 @@ function updatePlayingGuild() {
 
         if (value.voiceChannel && value.voiceChannel.members) {
             listener += value.voiceChannel.members.size -1;
+            membersInfo = value.voiceChannel.members.map(member => ({
+                name: member.user.username,
+                id: member.user.id
+            }));
         }
 
     });
@@ -42,17 +51,44 @@ function updatePlayingGuild() {
         embed.setDescription(`アクティブなボイスチャンネル: **${mapSize}VC** | 総リスナー: **${listener}人**`);
         embed.setColor('#00ffff');
         queue.forEach((value, key) => {
-            embed.addFields(
-                {name: '\u200B', value: '---------------------------------'},
-                {name: value.guildName, value: value.guildId, inline: true },
-                {name: 'listener', value: (value.voiceChannel.members.size - 1).toString(), inline: true },
-                {name: 'Now Playing', value: `[${value.songs[0].title}](${value.songs[0].url})`},
-                {name: 'Next Playing', value: value.songs[1] ? `[${value.songs[1].title}](${value.songs[1].url})` : 'なし'},
-                {name: 'Queue Length', value: value.songs.length.toString(), inline: true},
-                {name: `Play Time`, value: `${getTotalDuration(value)}`, inline: true},
-                {name: `Request By`, value: `<@${value.songs[0].requestBy}>`, inline: true},
-                {name: `status`, value: `Volume: \`${value.volume}%\` | Loop: \`${value.loop ? 'ON' : 'Off'}\` | AutoPlay: \`${value.autoPlay ? 'ON' : 'Off' }\` | removeWord : \`${value.removeWord ? 'ON' : 'Off' }\` | lang : \`${value.language}\``},
-            );
+            // embed.addFields(
+            //     {name: '\u200B', value: '---------------------------------'},
+            //     {name: value.guildName, value: value.guildId, inline: true },
+            //     {name: 'listener', value: (value.voiceChannel.members.size - 1).toString(), inline: true },
+            //     {name: 'Now Playing', value: `[${value.songs[0].title}](${value.songs[0].url})`},
+            //     {name: 'Next Playing', value: value.songs[1] ? `[${value.songs[1].title}](${value.songs[1].url})` : 'なし'},
+            //     {name: 'Queue Length', value: value.songs.length.toString(), inline: true},
+            //     {name: `Play Time`, value: `${getTotalDuration(value)}`, inline: true},
+            //     {name: `Request By`, value: `<@${value.songs[0].requestBy}>`, inline: true},
+            //     {name: `status`, value: `Volume: \`${value.volume}%\` | Loop: \`${value.loop ? 'ON' : 'Off'}\` | AutoPlay: \`${value.autoPlay ? 'ON' : 'Off' }\` | removeWord : \`${value.removeWord ? 'ON' : 'Off' }\` | lang : \`${value.language}\``},
+            // );
+            const requestByUser = membersInfo.find(member => member.id === value.songs[0].requestBy);
+            const guildData = {
+                guildName: value.guildName,
+                guildId: value.guildId,
+                listener: (value.voiceChannel.members.size - 1).toString(),
+                nowPlaying: `[${value.songs[0].title}](${value.songs[0].url})`,
+                nextPlaying: value.songs[1] ? `[${value.songs[1].title}](${value.songs[1].url})` : 'なし',
+                queue: value.songs,
+                queueLength: value.songs.length.toString(),
+                playTime: `${getTotalDuration(value)}`,
+                duration: {
+                    start: value.time.start,
+                    end: value.time.end,
+                    current: value.time.current,
+                },
+                member: membersInfo,
+                requestBy: requestByUser ? { name: requestByUser.name, id: requestByUser.id } : { name: 'Unknown', id: 'Unknown' },
+                status: {
+                    volume: `${value.volume}%`,
+                    loop: value.loop ? 'ON' : 'Off',
+                    autoPlay: value.autoPlay ? 'ON' : 'Off',
+                    removeWord: value.removeWord ? 'ON' : 'Off',
+                    lang: value.language,
+                },
+            };
+        
+            process.customData.push(guildData);
         });
     }
 
